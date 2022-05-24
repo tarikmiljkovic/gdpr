710ea85f87eefc08f0a8398a8fd3effdb31d756c
Miljkovic <tarik.miljkovic@stud.fh-campuswien.ac.at>
2022-05-24 14:35:08 +0200

    link cov2

--- components/LangSwitcher.js
+++ components/LangSwitcher.js
@@ -30,37 +30,44 @@ function LangSwitcher() {
 
   return (
     <span className="relative z-0 inline-flex py-4">
-      <button
-        type="button"
-        className="relative inline-flex items-center px-4 py-2  border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+      <Link
+        href={{
+          pathname: "/generate",
+          query: {
+            usersFullName: fullName,
+            usersMeans: means,
+            usersRequest: request,
+            usersAddress: address,
+          },
+        }}
+        locale={(router.locale = "de")}
       >
-        <Link href={{
-                    pathname: "/generate",
-                    query: {
-                      usersFullName: fullName,
-                      usersMeans: means,
-                      usersRequest: request,
-                      usersAddress: address,
-                    }}} locale={(router.locale = "de")}  >
+        <button
+          type="button"
+          className="relative inline-flex items-center px-4 py-2  border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        >
           <a>Deutsch</a>
-        </Link>
-      </button>
-      <button
-        type="button"
-        className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        </button>
+      </Link>
+      <Link
+        href={{
+          pathname: "/generate",
+          query: {
+            usersFullName: fullName,
+            usersMeans: means,
+            usersRequest: request,
+            usersAddress: address,
+          },
+        }}
+        locale={(router.locale = "en")}
       >
-        <Link href={{
-                    pathname: "/generate",
-                    query: {
-                      usersFullName: fullName,
-                      usersMeans: means,
-                      usersRequest: request,
-                      usersAddress: address,
-                    }}} locale={(router.locale = "en")}  >
+        <button
+          type="button"
+          className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+        >
           <a>English</a>
-        </Link>
-      </button>
-
+        </button>
+      </Link>
     </span>
   );
 }
