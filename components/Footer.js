f3dff107c1dee7033b3b1a57bc4ea036cf4062af
Miljkovic <tarik.miljkovic@stud.fh-campuswien.ac.at>
2022-05-24 10:38:21 +0200

    enables 7 requests and translations

--- components/Footer.js
+++ components/Footer.js
@@ -65,7 +65,7 @@ const navigation = {
           IT Team: Tarik Miljkovic, Elmi Abdullahi, Turgut Aydemir
         </p>
         <p className="mt-0 text-center text-base text-gray-400">
-          Law Team: Julia Damian, Vasilje, Josef
+          Law Team: Julia Damian, Vasilije Skopelja, Josef
         </p>
       </div>
     </footer>
